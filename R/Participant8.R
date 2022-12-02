55ed8433eaa0d405b53016702433daaf70897b9d
runlinwang <rwang3@college.harvard.edu>
2022-12-02 02:56:35 -0500

    benji done

--- R/Participant8.R
+++ R/Participant8.R
@@ -10,31 +10,33 @@ mtcars %>%
   filter(hp > 100) %>%
   count()
 
-# 23
+
 
 # Question 1: What is the average (mean) Wind speed in the dataset?
 
 airquality %>%
-  summarise(avg=mean(Wind))
+  summarize(mean(Wind))
 
-# 9.958
+#9.957516
 
 
 # Question 2: What is the average (mean) temperature across May and June?
 
 airquality %>%
-  filter(Month >= 5 && Month <= 6) %>%
-  summarise(avg=mean(Temp))
+  group_by(Month) %>%
+  summarize(Temp=mean(Temp)) %>%
+  filter(Month<7) %>%
+  summarize(b = mean(Temp))
 
-# 77.882
+# 72.3
 
 # Question 3: What is the maximum wind speed during August?
 
 airquality %>%
-  filter(Month == 8) %>%
-  summarise(max_wind=max(Wind))
+  group_by(Month=8) %>%
+  summarize(max(Wind))
 
-# 15.5
+# 20.7
 
 
 # Question 4: Create a new column called “Hot” which is 1 if Temp is above 80 and 0 otherwise.
@@ -42,8 +44,9 @@ airquality %>%
 # hint: consider using the ifelse() function in R
 
 airquality %>%
-  mutate(Hot=ifelse(Temp > 80, 1, 0)) %>%
-  summarise(sum(Hot))
+  mutate(Hot=ifelse(Temp>80, 1, 0)) %>%
+  filter(Hot==0) %>%
+  count()
 
-# 68
+#85
 
