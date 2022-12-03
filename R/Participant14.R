3036434d757581a3b0191744fe0ef573c0cfbc4e
runlinwang <rwang3@college.harvard.edu>
2022-12-02 23:17:10 -0500

    kat p14

---
+++ R/Participant14.R
@@ -0,0 +1,37 @@
+data(mtcars)
+data(airquality)
+library(dplyr)
+library(tidyverse)
+
+# Warmup: Please use piping in order to find the number of cars in the
+# mtcars dataset with horsepower of over 100.
+
+mtcars %>% filter(hp > 100) %>% nrow
+
+
+
+# Question 1: What is the average (mean) Wind speed in the dataset?
+
+airquality
+# 9.96
+
+
+# Question 2: What is the average (mean) temperature across May and June?
+
+airquality %>% filter(Month == 5 | Month == 6)
+# 10.96
+
+
+# Question 3: What is the maximum wind speed during August?
+
+airquality %>% filter(Month == 8)
+# 15.50
+
+
+# Question 4: Create a new column called “Hot” which is 1 if Temp is above 80 and 0 otherwise.
+# (Upload a screenshot of your dataset.) How many cold observations are there?
+# hint: consider using the ifelse() function in R
+
+airquality %>% mutate(Hot = if_else(Temp > 80, 1, 0)) %>% filter(Hot == 0) %>% count
+# 85
+
